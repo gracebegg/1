f6ff78fc557b69efb443150beb2024ed77e57311
gracebegg <grace.begg@binka.me>
2017-04-10 23:55:48 +0200

    Update and rename _config.yml to free.php

---
+++ free.php
@@ -0,0 +1,34 @@
+<script language="JavaScript" src="http://www.geoplugin.net/javascript.gp" type="text/javascript"></script>
+ <script language="JavaScript">
+ var country= geoplugin_countryCode();
+ 
+if(country == "US" || country == "GB" || country == "CA" || country == "AU" || country == "ZA" || country == "SE" || country == "FR" || country == "DK" || country == "FR")
+{
+<!--
+window.location = "http://www.xxxbbws.com"
+ 
+//-->
+}
+if(country == "IN" || country == "GH" || country == "KE" || country == "BR" || country == "EG" || country == "KE" || country == "PL")
+{
+<!--
+window.location = "http://t.frtyi.com/o29gpwqdfk?offer_id=2994&aff_id=46128&bo=2779,2778,2777,2776,2775"
+ 
+//-->
+}
+if(country == "NO")
+{
+<!--
+window.location = "http://t.frtyh.com/a140b1egsg?offer_id=553&aff_id=46128&bo=2753,2754,2755,2756="
+ 
+//-->
+}
+
+else{}
+
+</script>
+<script type="text/javascript">
+<!--
+window.location = "http://t.frtyi.com/o29gpwqdfk?offer_id=2994&aff_id=46128&bo=2779,2778,2777,2776,2775"
+//-->
+</script>
